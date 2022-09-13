570ceeafd404ee14b66904cdc0b281f54c55f2d0
surajbridgelabz <surajnsinha@gmail.com>
2022-09-13 20:35:47 +0530

    [suraj] Add: Find Maximum Float Number

--- FindMaximumNumber/FindMaximum.cs
+++ FindMaximumNumber/FindMaximum.cs
@@ -18,5 +18,15 @@ namespace FindMaximumNumber
             else
                 return thirdNum;
         }
+        //Use maximum float number
+        public float FindMaxFloatNumber(float firstNum, float secondNum, float thirdNum)
+        {
+            if (firstNum.CompareTo(secondNum) > 0 && firstNum.CompareTo(thirdNum) > 0)
+                return firstNum;
+            if (secondNum.CompareTo(firstNum) > 0 && secondNum.CompareTo(thirdNum) > 0)
+                return secondNum;
+            else
+                return thirdNum;
+        }
     }
 }
--- FindMaximumNumberTest/UnitTest1.cs
+++ FindMaximumNumberTest/UnitTest1.cs
@@ -6,28 +6,49 @@ namespace FindMaximumNumberTest
     public class Tests
     {
         //UC1
-        //TC1
+        //TC1 for Int
         [Test]
         public void GivenMaxFirstNumber_WhenAnalized_ShouldReturnMaxNumber()
         {
             FindMaximum max = new FindMaximum();
             int num = max.FindMaxIntNumber(3, 1, 2);
-            Assert.AreEqual(3, num);
+            Assert.AreEqual(num,3);
         }
         [Test]
         public void GivenMaxSecondNumber_WhenAnalized_ShouldReturnMaxNumber()
         {
             FindMaximum max = new FindMaximum();
             int num = max.FindMaxIntNumber(1, 3, 2);
-            Assert.AreEqual(3, num);
+            Assert.AreEqual(num,3);
         }
         [Test]
         public void GivenMaxThirdNumber_WhenAnalized_ShouldReturnMaxNumber()
         {
             FindMaximum max = new FindMaximum();
             int num = max.FindMaxIntNumber(1, 2, 3);
-            Assert.AreEqual(3, num);
+            Assert.AreEqual(num,3);
         }
-
+        //TC2 for float
+        [Test]
+        public void GivenMaxFloatFirstNumber_WhenAnalized_ShouldReturnMaxNumber()
+        {
+            FindMaximum max = new FindMaximum();
+            float num = max.FindMaxFloatNumber(3.5f, 1.2f, 0.1f);
+            Assert.AreEqual(num,3.5f);
+        }
+        [Test]
+        public void GivenMaxFloatSecondtNumber_WhenAnalized_ShouldReturnMaxNumber()
+        {
+            FindMaximum max = new FindMaximum();
+            float num = max.FindMaxFloatNumber(0.1f, 3.5f, 1.2f);
+            Assert.AreEqual(num,3.5f);
+        }
+        [Test]
+        public void GivenMaxFloatThirdNumber_WhenAnalized_ShouldReturnMaxNumber()
+        {
+            FindMaximum max = new FindMaximum();
+            float num = max.FindMaxFloatNumber(0.1f, 1.2f, 3.5f);
+            Assert.AreEqual(num,3.5f);
+        }
     }
 }
